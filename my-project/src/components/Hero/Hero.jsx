2150338a431ddb6b38f07d6d4537442120e92e25
inilogicz <jiajala@student.lautech.edu.ng>
2025-01-09 01:26:49 +0100

    first commit

---
+++ my-project/src/components/Hero/Hero.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import carImage from '../../assets/car.png'; 
+import buildingImage from '../../assets/building.jpg';
+import{FaArrowRight} from 'react-icons/fa'
+
+
+const bgImage = {
+  bacgroundImage: `url(${buildingImage})`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundPosition: "center", 
+   
+};
+
+const Hero = () => {
+  return (
+    <main style={bgImage}>
+      <div className='bg-gradient-to-r from-primary to bg-primaryDark/50'>
+        <div className="container text-white" >
+            {/* {navbar components} */}
+
+            {/* {Hero section} */}
+            
+            <section className='grid grid-cols-1 md:grid-cols-2 place-items-center min-h-[800px] '>
+                {/* {textarea} */}
+
+            <div className='space-y-8'> 
+                <h1 classname=" text-5xl lg:text-7xl  font-bold leading-tight ">
+                   Honda <br /> {" "}
+                      <span className=' text-transparent text-outline '>
+                        Sports EV
+                      </span>
+                </h1>
+                <button className='outline-btn flex justify-center  items-center gap-4 group'>
+                    Explore
+                    <FaArrowRight
+                    className='group-hover:translate-x-2 transition'/>
+                </button>
+
+                <div className='bg-black/40 p-6 space-y-2 lg:max-w-[350px] !mt=[60px]'>
+                  {/* {features section} */}
+                  <h1 className="uppercase text-xs leading loose">
+                    Vehicle features
+                   
+                  </h1>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore est voluptas harum delectus asperiores voluptate nemo quasi nulla sint porro! Sit architecto voluptates nulla numquam expedita nam, sequi excepturi ratione!</p>
+                </div>
+            </div> 
+                {/* {image area} */}
+                <div>
+                    <img src={carImage} alt="" />
+                </div>
+            </section>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Hero;
+
+
+
+
+
+
+
+
